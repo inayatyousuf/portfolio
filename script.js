59be224e7fa891066f72a5b7a620ba504934bc02
inayat yousuf <inayatyousuf77@gmail.com>
2022-01-03 19:44:34 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+// const links = document.querySelectorAll('.link');
+// const sections = document.querySelectorAll('section');
+
+// // console.log(sections)
+// let activeLink = 0;
+//  links.forEach((link,i)=>{
+//      link.addEventListener('click', () => {
+//          if(activeLink != i){
+//             //  console.log(i,activeLink)
+//              links[activeLink].classList.remove('active');
+//              link.classList.add('active')
+//         sections[activeLink].classList.remove('active');
+             
+
+//              setTimeout(() => {
+//                   activeLink = i;
+//                   sections[i].classList.add['active']
+//              },1000);
+//          }
+//      })
+//  })
+
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})
